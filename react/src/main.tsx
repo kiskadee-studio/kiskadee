4c7b98f1c671d4db60ef58c4c4825d3eb1dd7858
Rodrigo Mello <rodrigojcmello@gmail.com>
2024-09-29 15:24:22 -0300

    fix: lint issue related to getElementById possibly being null

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+const root = document.getElementById('root') as HTMLElement;
+
+createRoot(root).render(
   <StrictMode>
     <App />
   </StrictMode>
