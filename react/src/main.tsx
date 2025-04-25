f184bcdb401743d3695cb79becfd6bdd85220144
Rodrigo Mello <rodrigojcmello@gmail.com>
2025-04-24 21:45:58 -0300

    chore: update vite and dependencies

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,12 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-const root = document.getElementById('root') as HTMLElement;
-
-createRoot(root).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>
-);
+  </StrictMode>,
+)
