e6a39adb12170409a88677377c9d7d5773fc4830
Rodrigo Mello <rodrigojcmello@gmail.com>
2025-06-13 11:19:13 -0300

    refactor: add exports for transform functions in appearance index

---
+++ packages/web-builder/src/phrase-2-convert-style-key-to-css/appearance/index.ts
@@ -0,0 +1,7 @@
+export * from './transformBorderStyleKeyToCss';
+export * from './transformShadowKeyToCss';
+export * from './transformTextAlignKeyToCss';
+export * from './transformTextWeightKeyToCss';
+export * from './transformTextDecorationKeyToCss';
+export * from './transformTextDecorationKeyToCss';
+export * from './transformTextItalicKeyToCss';
