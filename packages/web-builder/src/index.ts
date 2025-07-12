370f7339be38fdf1df3b5f475e01864b52294220
Rodrigo Mello <rodrigojcmello@gmail.com>
2025-07-12 10:28:43 -0300

    refactor: rename files for style key conversion phase 3

--- packages/web-builder/src/index.ts
+++ packages/web-builder/src/index.ts
@@ -1,6 +1,6 @@
 import { schema, type InteractionState } from '@kiskadee/schema';
 import { convertElementSchemaToStyleKeys } from './phase-1-convert-schema-to-style-keys/convertElementSchemaToStyleKeys';
-import { generateCssFromStyleKeyList } from './phrase-3-convert-style-keys-to-css-rules/generateCss';
+import { generateCssFromStyleKeyList } from './phase-3-convert-style-keys-to-css-rules/generateCss';
 
 // Phase 1 - Convert Element Schema to Style Kyes
 const styleKeyList = convertElementSchemaToStyleKeys(schema);
@@ -9,6 +9,8 @@ const styleKeyList = convertElementSchemaToStyleKeys(schema);
 
 // Phase 3 - Convert Style Keys to CSS rules
 // const cssGenerated = await generateCssFromStyleKeyList(styleKeyList);
+
+// Phase 4 - Shorten CSS class names
 
 // Output
 console.log({ styleKeyList });
