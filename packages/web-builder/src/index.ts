53181d05531b884b98ba6ceee56da5636fb8acf1
Rodrigo Mello <rodrigojcmello@gmail.com>
2025-07-12 10:25:49 -0300

    refactor: update comments for style key processing phases

--- packages/web-builder/src/index.ts
+++ packages/web-builder/src/index.ts
@@ -2,10 +2,12 @@ import { schema, type InteractionState } from '@kiskadee/schema';
 import { convertElementSchemaToStyleKeys } from './phrase-1-convert-schema-to-style-keys/convertElementSchemaToStyleKeys';
 import { generateCssFromStyleKeyList } from './phrase-2-convert-style-key-to-css/generateCss';
 
-// Phrase 1
+// Phase 1 - Convert Element Schema to Style Kyes
 const styleKeyList = convertElementSchemaToStyleKeys(schema);
 
-// Phrase 2
+// Phase 2 - Map style key usage for optimization purposes
+
+// Phase 3 - Convert Style Keys to CSS rules
 // const cssGenerated = await generateCssFromStyleKeyList(styleKeyList);
 
 // Output
