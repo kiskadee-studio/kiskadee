f151cf670c4a11c425e8eba89b10ba635077c300
Rodrigo Mello <rodrigojcmello@gmail.com>
2025-08-17 16:16:18 -0300

    refactor: restructure transformBorderKeyToCss tests for clarity

--- packages/web-builder/src/phase-4-convert-style-keys-to-css-rules/appearance/transformBorderKeyToCss.test.ts
+++ packages/web-builder/src/phase-4-convert-style-keys-to-css-rules/appearance/transformBorderKeyToCss.test.ts
@@ -5,43 +5,74 @@ import { transformBorderKeyToCss } from './transformBorderKeyToCss';
 
 const propertyName = 'borderStyle';
 const className = 'abc';
+const { dashed, solid, dotted, none }: typeof CssBorderStyleValue = CssBorderStyleValue;
 
 describe('transformBorderToCss', () => {
   describe('Successful operation', () => {
-    for (const [keySuffix, cssValue] of Object.entries(CssBorderStyleValue)) {
-      const styleKey = `${propertyName}__${keySuffix}`;
+    it('returns expected CSS rule for "borderStyle__none"', () => {
+      const styleKey = `${propertyName}__${none}`;
+      const result = transformBorderKeyToCss(styleKey, className);
 
-      it(`should return correct css rule for "${keySuffix}"`, () => {
-        const result = transformBorderKeyToCss(styleKey, className);
+      expect(result).toEqual('.abc { border-style: none }');
+      expect(result).toMatchSnapshot();
+    });
 
-        expect(result).toEqual(`.abc { border-style: ${cssValue} }`);
-      });
-    }
+    it('returns expected CSS rule for "borderStyle__dotted"', () => {
+      const styleKey = `${propertyName}__${dotted}`;
+      const result = transformBorderKeyToCss(styleKey, className);
+
+      expect(result).toEqual('.abc { border-style: dotted }');
+      expect(result).toMatchSnapshot();
+    });
+
+    it('returns expected CSS rule for "borderStyle__dashed"', () => {
+      const styleKey = `${propertyName}__${dashed}`;
+      const result = transformBorderKeyToCss(styleKey, className);
+
+      expect(result).toEqual('.abc { border-style: dashed }');
+      expect(result).toMatchSnapshot();
+    });
+
+    it('returns expected CSS rule for "borderStyle__solid"', () => {
+      const styleKey = `${propertyName}__${solid}`;
+      const result = transformBorderKeyToCss(styleKey, className);
+
+      expect(result).toEqual('.abc { border-style: solid }');
+      expect(result).toMatchSnapshot();
+    });
   });
 
   describe('Error handling', () => {
-    it('should throw if the property segment is unsupported', () => {
+    it('throws for unsupported property segment', () => {
       const badKey = 'invalidProperty__solid';
       const expectedError = UNSUPPORTED_PROPERTY('invalidProperty', badKey);
+
       expect(() => transformBorderKeyToCss(badKey, className)).toThrowError(expectedError);
+      expect(expectedError).toMatchSnapshot();
     });
 
-    it('should throw if the value is not supported', () => {
+    it('throws for unsupported value segment', () => {
       const badKey = `${propertyName}__unknownValue`;
       const expectedError = UNSUPPORTED_VALUE(propertyName, 'unknownValue', badKey);
+
       expect(() => transformBorderKeyToCss(badKey, className)).toThrowError(expectedError);
+      expect(expectedError).toMatchSnapshot();
     });
 
-    it('should throw if the value is missing (no value segment provided)', () => {
+    it('throws when value segment is missing', () => {
       const badKey = `${propertyName}`;
       const expectedError = UNSUPPORTED_VALUE(propertyName, undefined as unknown as string, badKey);
+
       expect(() => transformBorderKeyToCss(badKey, className)).toThrowError(expectedError);
+      expect(expectedError).toMatchSnapshot();
     });
 
-    it('should throw if multiple value segments are provided', () => {
+    it('throws when multiple value segments are provided', () => {
       const badKey = `${propertyName}__solid__extra`;
       const expectedError = UNSUPPORTED_VALUE(propertyName, 'solid__extra', badKey);
+
       expect(() => transformBorderKeyToCss(badKey, className)).toThrowError(expectedError);
+      expect(expectedError).toMatchSnapshot();
     });
   });
 });
