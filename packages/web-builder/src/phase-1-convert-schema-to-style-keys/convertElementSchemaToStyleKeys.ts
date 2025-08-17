300a18c4c70d766b1a0f56574af17b950960be28
Rodrigo Mello <rodrigojcmello@gmail.com>
2025-08-17 09:55:43 -0300

    refactor: reorder imports and remove redundant logging

--- packages/web-builder/src/phase-1-convert-schema-to-style-keys/convertElementSchemaToStyleKeys.ts
+++ packages/web-builder/src/phase-1-convert-schema-to-style-keys/convertElementSchemaToStyleKeys.ts
@@ -4,11 +4,11 @@ import type {
   Schema,
   StyleKeyByElement
 } from '@kiskadee/schema';
-import { convertElementDecorationsToStyleKeys } from './decoration/convertElementDecorationsToStyleKeys';
-import { convertElementColorsToStyleKeys } from './colors/convertElementColorsToStyleKeys';
-import { convertElementScalesToStyleKeys } from './scales/convertElementScalesToStyleKeys';
-import { convertElementShadowToStyleKeys } from './effects/convertElementShadowToStyleKeys';
 import { deepUpdate } from '../utils';
+import { convertElementColorsToStyleKeys } from './colors/convertElementColorsToStyleKeys';
+import { convertElementDecorationsToStyleKeys } from './decoration/convertElementDecorationsToStyleKeys';
+import { convertElementShadowToStyleKeys } from './effects/convertElementShadowToStyleKeys';
+import { convertElementScalesToStyleKeys } from './scales/convertElementScalesToStyleKeys';
 
 /**
  * Processes a Schema object by iterating over each component's elements.
@@ -52,12 +52,5 @@ export function convertElementSchemaToStyleKeys(schema: Schema): ComponentStyleK
     }
   }
 
-  console.log({ styleKeysByComponent: JSON.stringify(styleKeysByComponent, null, 2) });
-
   return styleKeysByComponent;
-
-  // // Log the final style usage map.
-  // return Object.fromEntries(
-  //   Object.entries(styleKeyUsageMap).sort(([a], [b]) => a.localeCompare(b))
-  // );
 }
