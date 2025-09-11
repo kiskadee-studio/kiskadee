d16a329101032a2a0dc9fe728d8a0a25a99797bb
Rodrigo Mello <rodrigojcmello@gmail.com>
2025-09-10 22:06:02 -0300

    refactor: add type annotations and clean up index.ts imports

--- packages/web-builder/src/index.ts
+++ packages/web-builder/src/index.ts
@@ -1,21 +1,27 @@
-import { schema, type InteractionState } from '@kiskadee/schema';
+import { type ComponentStyleKeyMap, schema } from '@kiskadee/schema';
 import { convertElementSchemaToStyleKeys } from './phase-1-convert-schema-to-style-keys/convertElementSchemaToStyleKeys';
+import {
+  mapStyleKeyUsage,
+  type StyleKeyUsageMap
+} from './phase-2-map-style-key-usage/mapStyleKeyUsage';
+import {
+  type ShortenCssClassNames,
+  shortenCssClassNames
+} from './phase-3-shorten-css-class-names/shortenCssClassNames';
 import { generateCssFromStyleKeyList } from './phase-4-convert-style-keys-to-css-rules/generateCss';
-import { mapStyleKeyUsage } from './phase-2-map-style-key-usage/mapStyleKeyUsage';
-import { shortenCssClassNames } from './phase-3-shorten-css-class-names/shortenCssClassNames';
 
 // Phase 1 - Convert Element Schema to Style Keys
-const styleKeys = convertElementSchemaToStyleKeys(schema);
+const styleKeys: ComponentStyleKeyMap = convertElementSchemaToStyleKeys(schema);
+// console.log({ styleKeys: JSON.stringify(styleKeys, null, 2) });
 
-// Phase 2 - Map style key usage for optimization purposes
-const styleKeyUsage = mapStyleKeyUsage(styleKeys);
+// // Phase 2 - Map style key usage for optimization purposes
+const styleKeyUsage: StyleKeyUsageMap = mapStyleKeyUsage(styleKeys);
+// console.log({ styleKeyUsage });
 
 // Phase 3 - Shorten CSS class names for optimization purposes
-const shortenCssClassNameMap = shortenCssClassNames(styleKeyUsage);
+const shortenCssClassNameMap: ShortenCssClassNames = shortenCssClassNames(styleKeyUsage);
+// console.log({ shortenCssClassNameMap });
 
 // Phase 4 - Convert Style Keys to CSS rules
-// const cssGenerated = await generateCssFromStyleKeyList(styleKeyList);
-
-// Output
-console.log({ styleKeyUsage, shortenCssClassNameMap });
-// console.log({ cssGenerated });
+const cssGenerated: string = await generateCssFromStyleKeyList(shortenCssClassNameMap);
+console.log({ cssGenerated });
