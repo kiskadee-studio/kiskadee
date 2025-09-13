bf724d82beb10dc90271bb44f4b3586547271c97
Rodrigo Mello <rodrigojcmello@gmail.com>
2025-09-13 16:10:33 -0300

    feat: add initial template schema with button styling

---
+++ packages/schema/src/templates/template-1.ts
@@ -0,0 +1,118 @@
+import { breakpoints } from '../breakpoints';
+import type { Schema } from '../schema';
+
+export const schema: Schema = {
+  breakpoints,
+  components: {
+    button: {
+      elements: {
+        e1: {
+          decorations: {
+            textItalic: true,
+            textWeight: 'bold',
+            textLineType: 'underline',
+            // textTransform: 'uppercase',
+            textAlign: 'center',
+            // cursor: 'pointer',
+            borderStyle: 'solid'
+
+            // TODO: maybe
+            // verticalAlign: 'middle',
+            // userSelect: 'none',
+            // whiteSpace: 'nowrap',
+            // overflow: 'hidden',
+            // textOverflow: 'ellipsis',
+          },
+          scales: {
+            textSize: {
+              's:sm:1': 12,
+              's:md:1': {
+                'bp:all': 16,
+                'bp:lg:1': 14
+              }
+            },
+            paddingTop: 10,
+            paddingRight: 8,
+            paddingBottom: 8,
+            paddingLeft: 8,
+            marginTop: 8,
+            marginRight: 16,
+            marginBottom: 8,
+            marginLeft: 16,
+            boxHeight: {
+              's:md:1': 40,
+              's:lg:1': {
+                'bp:all': 48,
+                'bp:lg:1': 44
+              }
+            },
+            boxWidth: 120,
+            borderWidth: 1,
+            borderRadius: 4,
+            textHeight: 24
+          },
+          palettes: {
+            p1: {
+              textColor: { rest: [0, 0, 0, 0.5] },
+              borderColor: { rest: [0, 0, 0, 0.5] },
+              boxColor: {
+                primary: {
+                  rest: [10, 35, 100, 0],
+                  hover: [10, 35, 100, 0]
+                },
+                danger: {
+                  rest: [10, 35, 100, 0],
+                  hover: [10, 35, 100, 0]
+                }
+                // instagram: {
+                //   rest: [10, 35, 100, 0],
+                //   hover: [10, 35, 100, 0],
+                //   active: [10, 35, 100, 0]
+                // }
+              }
+            },
+            p2: {
+              boxColor: { rest: [0, 0, 0, 0.5] }
+            }
+          },
+          effects: {
+            shadow: {
+              color: { rest: [0, 0, 0, 0.5] },
+              blur: {
+                // TODO: setting rest makes sense for shadow?
+                rest: 0,
+                hover: 4
+              },
+              y: {
+                rest: 0,
+                hover: 4
+              },
+              x: {
+                rest: 0,
+                hover: 4
+              }
+            }
+          }
+        },
+        e2: {
+          decorations: {
+            textItalic: true
+          },
+          scales: {
+            textSize: 16
+          },
+          palettes: {
+            p1: {
+              textColor: {
+                primary: {
+                  rest: [0, 0, 0, 0.5],
+                  hover: { ref: [0, 0, 0, 0.5] }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+};
