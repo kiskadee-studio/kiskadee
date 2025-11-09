25323d1de2b5caa0a2fe739c8dfadd5e9a40ffd7
Rodrigo Mello <rodrigojcmello@gmail.com>
2025-11-09 10:06:32 -0300

    refactor: add BackgroundTonePicker component and related styles

--- packages/components/react/src/App.tsx
+++ packages/components/react/src/App.tsx
@@ -1,6 +1,7 @@
 import { Link, Route, Routes } from 'react-router';
 import './App.css';
 
+import BackgroundTonePicker from './components/BackgroundTonePicker';
 import ButtonPage from './pages/Button/Button.tsx';
 import TabsPage from './pages/Tabs';
 import TextPage from './pages/Text';
@@ -8,6 +9,7 @@ import TextPage from './pages/Text';
 function App() {
   return (
     <>
+      <BackgroundTonePicker />
       <h1>Componentes</h1>
       <nav style={{ display: 'flex', gap: 12 }}>
         <Link to="/button">Button</Link>
